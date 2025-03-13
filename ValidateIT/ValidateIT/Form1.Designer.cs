391f0109722ec6eb1faf8c8c3eaad632556b88eb
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-03-13 16:21:27 -0700

    Started work in the GUI

--- ValidateIT/ValidateIT/Form1.Designer.cs
+++ ValidateIT/ValidateIT/Form1.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            generateBtn = new Button();
+            SuspendLayout();
+            // 
+            // generateBtn
+            // 
+            generateBtn.Location = new Point(56, 130);
+            generateBtn.Name = "generateBtn";
+            generateBtn.Size = new Size(112, 34);
+            generateBtn.TabIndex = 0;
+            generateBtn.Text = "Generate";
+            generateBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(generateBtn);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button generateBtn;
     }
 }
